0535125ab685183c729d7d68eac27c33eee662cd
Bob Wallis <bob.wallis@gmail.com>
2015-06-20 10:17:01 +0100

    Wait for page load before using <canvas> to try and resolve white out issue. Then don't convert Canvas to images as it's quicker not to.

--- app/src/webviews/js/grids.js
+++ app/src/webviews/js/grids.js
@@ -44,53 +44,55 @@ require(['jquery', 'Method', 'Grid'], function( $, Method, Grid ) {
     var container = document.getElementById( 'container' ),
     	method = new Method( qs );
 
-	// Plain course
-	var plainCourseGrid = new Grid( $.extend( true, method.gridOptions.plainCourse[qs.type], {
-		title: false,
-		sideNotation: {
-			show: true,
-			color: '#999',
-			font: (qs.fontSize*0.75)+'px sans-serif'
-		},
-		layout: {
-			numberOfColumns: (qs.layout == 'oneRow')? method.numberOfLeads: 1,
-			numberOfLeads: (qs.type == 'grid')? 1 : method.numberOfLeads
-		}
-	} ) ),
-		plainCourseGridImage = plainCourseGrid.draw(true);
-	plainCourseContainer = document.createElement( 'div' );
-	plainCourseContainer.appendChild( plainCourseGridImage );
-	container.appendChild( plainCourseContainer );
+    window.addEventListener( 'load', function() {
+		// Plain course
+		var plainCourseGrid = new Grid( $.extend( true, method.gridOptions.plainCourse[qs.type], {
+			title: false,
+			sideNotation: {
+				show: true,
+				color: '#999',
+				font: (qs.fontSize*0.75)+'px sans-serif'
+			},
+			layout: {
+				numberOfColumns: (qs.layout == 'oneRow')? method.numberOfLeads: 1,
+				numberOfLeads: (qs.type == 'grid')? 1 : method.numberOfLeads
+			}
+		} ) ),
+			plainCourseGridImage = plainCourseGrid.draw();
+		plainCourseContainer = document.createElement( 'div' );
+		plainCourseContainer.appendChild( plainCourseGridImage );
+		container.appendChild( plainCourseContainer );
 
-	var plainCourseSideNotationPadding = ((plainCourseGrid.measure().column.padding.right||0) - (plainCourseGrid.measure().canvas.padding.left||0))
-	plainCourseGridImage.style.marginLeft = plainCourseSideNotationPadding+'px';
-	if(qs.layout == 'oneRow') {
-		plainCourseContainer.style.marginLeft = (-plainCourseSideNotationPadding)+'px';
-	}
-
-	// Calls
-	method.gridOptions.calls[qs.type].map( function(e) {
-		var callContainer = document.createElement( 'div' ),
-			title = document.createElement( 'h1' ),
-			titleText = document.createTextNode( e.title.text.replace(':', '') ),
-			callGrid = new Grid( $.extend( e, {
-				sideNotation: {
-					show: true,
-					color: '#999',
-					font: (qs.fontSize*0.75)+'px sans-serif'
-				},
-				title: false
-			} ) ),
-			callGridImage = callGrid.draw(true);
-		title.appendChild( titleText );
-		callContainer.appendChild( title );
-		callContainer.appendChild( callGridImage );
-		container.appendChild( callContainer );
-
-		var callSideNotationPadding = ((callGrid.measure().column.padding.right||0) - (callGrid.measure().canvas.padding.left||0));
-		callGridImage.style.marginLeft = callSideNotationPadding+'px';
+		var plainCourseSideNotationPadding = ((plainCourseGrid.measure().column.padding.right||0) - (plainCourseGrid.measure().canvas.padding.left||0))
+		plainCourseGridImage.style.marginLeft = plainCourseSideNotationPadding+'px';
 		if(qs.layout == 'oneRow') {
-			callContainer.style.marginLeft = (-callSideNotationPadding)+'px';
+			plainCourseContainer.style.marginLeft = (-plainCourseSideNotationPadding)+'px';
 		}
+
+		// Calls
+		method.gridOptions.calls[qs.type].map( function(e) {
+			var callContainer = document.createElement( 'div' ),
+				title = document.createElement( 'h1' ),
+				titleText = document.createTextNode( e.title.text.replace(':', '') ),
+				callGrid = new Grid( $.extend( e, {
+					sideNotation: {
+						show: true,
+						color: '#999',
+						font: (qs.fontSize*0.75)+'px sans-serif'
+					},
+					title: false
+				} ) ),
+				callGridImage = callGrid.draw();
+			title.appendChild( titleText );
+			callContainer.appendChild( title );
+			callContainer.appendChild( callGridImage );
+			container.appendChild( callContainer );
+
+			var callSideNotationPadding = ((callGrid.measure().column.padding.right||0) - (callGrid.measure().canvas.padding.left||0));
+			callGridImage.style.marginLeft = callSideNotationPadding+'px';
+			if(qs.layout == 'oneRow') {
+				callContainer.style.marginLeft = (-callSideNotationPadding)+'px';
+			}
+		} );
 	} );
 } );
