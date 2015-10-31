ad533ca5990ccc1f9704c8c090f815bce4f6bba8
Bob Wallis <bob.wallis@gmail.com>
2015-10-31 15:54:52 +0000

    Add version to About page

--- app/src/main/java/uk/me/rsw/bl/fragments/AboutFragment.java
+++ app/src/main/java/uk/me/rsw/bl/fragments/AboutFragment.java
@@ -1,6 +1,7 @@
 package uk.me.rsw.bl.fragments;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
@@ -9,9 +10,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import uk.me.rsw.bl.BuildConfig;
 import uk.me.rsw.bl.R;
 
 public class AboutFragment extends Fragment {
@@ -21,11 +25,27 @@ public class AboutFragment extends Fragment {
     public AboutFragment() {
     }
 
+    public class WebAppInterface {
+        Context mContext;
+
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        @JavascriptInterface
+        public String versionName() {
+            return BuildConfig.VERSION_NAME;
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_nonfocusable_webview_in_card_and_nestedscrollview, container, false);
 
         mWebView = (WebView) view.findViewById(R.id.webview);
+        mWebView.addJavascriptInterface(new WebAppInterface(getActivity()), "Android");
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
         mWebView.setWebViewClient(new blWebViewClient());
         mWebView.loadUrl("file:///android_asset/webviews/about.html");
 
