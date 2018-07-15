2b998bf51a5a709d6f096eae28df0f962df4c737
Bob Wallis <bob.wallis@gmail.com>
2018-07-15 19:58:33 +0100

    Optimise imports

--- app/src/main/java/uk/me/rsw/bl/activities/CustomActivity.java
+++ app/src/main/java/uk/me/rsw/bl/activities/CustomActivity.java
@@ -3,7 +3,6 @@ package uk.me.rsw.bl.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
