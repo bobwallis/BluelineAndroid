607b1dbaee046bcbea8b2422a32a12110db59e01
Bob Wallis <bob.wallis@gmail.com>
2020-07-08 22:47:05 +0100

    Update NPM dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -4,90 +4,87 @@ var m = {
 	localStorage_age: '20190807'
 };
 
-var gulp            = require( 'gulp' );
-var rename          = require( 'gulp-rename' );
-var concat          = require( 'gulp-concat');
-var es              = require( 'event-stream' );
-var less            = require( 'gulp-less' );
-var autoprefixer    = require( 'gulp-autoprefixer' );
-var minifyCss       = require( 'gulp-minify-css' );
-var uncss           = require( 'gulp-uncss' );
-var imagemin        = require( 'gulp-imagemin' );
-var imagemin_zopfli = require( 'imagemin-zopfli' );
-var svg2png         = require( 'gulp-svg2png' );
-var requirejs       = require( 'gulp-requirejs' );
-var amdclean        = require( 'gulp-amdclean' );
-var uglify          = require( 'gulp-uglify' );
-var mustache        = require( 'gulp-mustache' );
-var typogr          = require( 'gulp-typogr' );
-var hypher          = require( 'gulp-hypher' );
-var h_pattern       = require( 'hyphenation.en-gb' );
-var htmlmin         = require( 'gulp-htmlmin' );
+var gulp         = require( 'gulp' );
+var rename       = require( 'gulp-rename' );
+var concat       = require( 'gulp-concat');
+var mergeStream  = require( 'merge-stream' );
+var less         = require( 'gulp-less' );
+var autoprefixer = require( 'gulp-autoprefixer' );
+var minifyCss    = require( 'gulp-minify-css' );
+var uncss        = require( 'gulp-uncss' );
+var imagemin     = require( 'gulp-imagemin' );
+var svg2png      = require( 'gulp-svg2png' );
+var requirejs    = require( 'gulp-requirejs' );
+var amdclean     = require( 'gulp-amdclean' );
+var uglify       = require( 'gulp-uglify' );
+var mustache     = require( 'gulp-mustache' );
+var typogr       = require( 'gulp-typogr' );
+var hypher       = require( 'gulp-hypher' );
+var h_pattern    = require( 'hyphenation.en-gb' );
+var htmlmin      = require( 'gulp-htmlmin' );
 
 
-gulp.task( 'default', ['html', 'css', 'js', 'js.workers'] );
-gulp.task( 'images', ['img', 'icon', 'icon-round', 'icon-store'] );
-
-
-gulp.task( 'html', function() {
-	gulp.src( SRC+'html/*.html' )
+function html() {
+	return gulp.src( SRC+'html/*.html' )
 		.pipe( mustache( m ) )
 		.pipe( typogr( { only: ['amp', 'widont', 'smartypants'] } ) )
 		.pipe( hypher( h_pattern ) )
 		.pipe( htmlmin( { removeComments: true, collapseWhitespace: true } ) )
 		.pipe( gulp.dest( DEST ) );
-} );
+};
 
 
-gulp.task( 'css', function() {
-	gulp.src( [SRC+'less/_.less'] )
+function css() {
+	return gulp.src( [SRC+'less/_.less'] )
 		.pipe( less() )
-		.pipe( autoprefixer( { browsers: ['Android > 0'] } ) )
+		.pipe( autoprefixer() )
 		.pipe( concat( 'blueline.css' ) )
 		.pipe( minifyCss( { keepSpecialComments: 0 } ) )
 		.pipe( gulp.dest( DEST ) );
-} );
+};
 
 
-gulp.task( 'img', function() {
-	gulp.src( [SRC+'img/*.svg'] )
-		.pipe( imagemin( { use: [imagemin_zopfli()] } ) )
-		.pipe( gulp.dest( DEST ) );
-	gulp.src( ['app/src/main/res/**/*.png'] )
-		.pipe( imagemin( { use: [imagemin_zopfli()] } ) )
-		.pipe( gulp.dest( 'app/src/main/res/' ) );
-} );
+function img() {
+	return mergeStream(
+		gulp.src( [SRC+'img/*.svg'] )
+			.pipe( imagemin() )
+			.pipe( gulp.dest( DEST ) ),
+		gulp.src( ['app/src/main/res/**/*.png'] )
+			.pipe( imagemin() )
+			.pipe( gulp.dest( 'app/src/main/res/' ) )
+	);
+};
 
 
-gulp.task( 'icon', function() {
+function icon() {
 	var tasks = [ ['mdpi',48], ['hdpi',72], ['xhdpi',96], ['xxhdpi',144], ['xxxhdpi',192] ].map( function( size ) {
 		return gulp.src( 'res/icon.svg' )
 			.pipe( svg2png( size[1]/192 ) )
-			.pipe( imagemin( { use: [imagemin_zopfli()] } ) )
+			.pipe( imagemin() )
 			.pipe( rename( 'ic_launcher.png' ) )
 			.pipe( gulp.dest( 'app/src/main/res/mipmap-'+size[0]+'/' ) );
 	} );
-	return es.merge.apply( null, tasks );
-} );
-gulp.task( 'icon-round', function() {
+	return mergeStream.apply( null, tasks );
+};
+function iconRound() {
 	var tasks = [ ['mdpi',48], ['hdpi',72], ['xhdpi',96], ['xxhdpi',144], ['xxxhdpi',192] ].map( function( size ) {
 		return gulp.src( 'res/icon_round.svg' )
 			.pipe( svg2png( size[1]/192 ) )
-			.pipe( imagemin( { use: [imagemin_zopfli()] } ) )
+			.pipe( imagemin() )
 			.pipe( rename( 'ic_launcher_round.png' ) )
 			.pipe( gulp.dest( 'app/src/main/res/mipmap-'+size[0]+'/' ) );
 	} );
-	return es.merge.apply( null, tasks );
-} );
-gulp.task( 'icon-store', function() {
+	return mergeStream.apply( null, tasks );
+};
+function iconStore() {
 	return gulp.src( 'res/icon_store.svg' )
 		.pipe( svg2png( 512/63 ) )
-		.pipe( imagemin( { use: [imagemin_zopfli()] } ) )
+		.pipe( imagemin() )
 		.pipe( rename( 'icon_full.png' ) )
 		.pipe( gulp.dest( 'res/' ) );
-} );
+};
 
-gulp.task( 'js', function() {
+function js() {
 	var tasks = ['grids', 'lines', 'custom', 'practice', 'prove', 'discover'].map( function( s ) {
 		return requirejs( {
 			baseUrl: SRC+'js/',
@@ -100,10 +97,14 @@ gulp.task( 'js', function() {
 			.pipe( uglify() )
 			.pipe( gulp.dest( DEST ) );
 	} );
-	return es.merge.apply( null, tasks );
-} );
+	return mergeStream.apply( null, tasks );
+};
 
-gulp.task( 'js.workers', function() {
-	gulp.src( [SRC+'js/gsiril.worker.js'] )
+function jsWorkers() {
+	return gulp.src( [SRC+'js/gsiril.worker.js'] )
 		.pipe( gulp.dest( DEST ) );
-} );
+};
+
+
+exports.default = gulp.parallel( html, css, js, jsWorkers );
+exports.images  = gulp.parallel( img, icon, iconRound, iconStore );
